e0c1dcfef92e18e77b46aaa97d13a1e777fddda5
Jeremy Jackson <jeremyj@thoughtworks.com>
2018-05-31 15:06:05 +0530

    [Jeremy] Minor refactoring

--- streaming.py
+++ streaming.py
@@ -1,31 +1,31 @@
 import tweepy
 import time
 import datetime
-
-consumer_token, consumer_secret, access_token, access_token_secret = open('tokens', 'r').read().split("\n")
-auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
-auth.set_access_token(access_token, access_token_secret)
-
-api = tweepy.API(auth)
-
+import os
 
 class StdOutListener(tweepy.StreamListener):
-
     def on_data(self, data):
-        # process stream data here
         print("streaming " + str(time.time()))
         open("data" + datetime.datetime.today().strftime('%Y-%m-%d') + ".json", "a+").write(data)
 
     def on_error(self, status):
         print(status)
 
+if __name__ == "__main__":
+    consumer_token = os.getenv("CONSUMER_TOKEN")
+    consumer_secret = os.getenv("CONSUMER_SECRET")
+    access_token = os.getenv("ACCESS_TOKEN")
+    access_token_secret = os.getenv("ACCESS_TOKEN_SECRET")
 
-listener = StdOutListener()
-myStream = tweepy.Stream(auth=api.auth, listener=listener)
+    auth = tweepy.OAuthHandler(consumer_token, consumer_secret)
+    auth.set_access_token(access_token, access_token_secret)
 
-print(dir(myStream))
-# myStream.sample()
-myStream.filter(track=[u"python", u"java", u"clojure", u"scala", u"haskell", u"javascript", u"babel", u"apache",
-                       u"nginx", u"redis", u"erlang", u"blockchain", u"golang", u"keras", u"tensorflow",
-                       u"deep learning", u"elixir", u"ruby", u"lua", u"c++", u"rustacean", u"lisp"
-                       ], async=True)
+    api = tweepy.API(auth)
+    listener = StdOutListener()
+
+    TweetStream = tweepy.Stream(auth=api.auth, listener=listener)
+
+    TweetStream.filter(track=[u"python", u"java", u"clojure", u"scala", u"haskell", u"javascript", u"babel", u"apache",
+                        u"nginx", u"redis", u"erlang", u"blockchain", u"golang", u"keras", u"tensorflow",
+                        u"deep learning", u"elixir", u"ruby", u"lua", u"c++", u"rustacean", u"lisp"
+                        ], async=True)
