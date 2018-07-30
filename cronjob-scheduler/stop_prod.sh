f9fd9043aab489f95d1c0ade3a102d7b78fcf6c8
Devashish Sood <devasood@gmail.com>
2018-07-30 13:07:36 +0530

    Add cronjobs

---
+++ cronjob-scheduler/stop_prod.sh
@@ -0,0 +1,7 @@
+instance=aws ec2 describe-instances | jq '.Reservations[].Instances | select(.[].Tags[].Value | startswith("Dory Prod") ) |
+  select(.[].Tags[].Key == "Name") |
+  {InstanceId: .[].InstanceId, PublicDnsName: .[].PublicDnsName, State: .[].State, LaunchTime: .[].LaunchTime, Tags: .[].Tags}
+  | [.]' | jq .[].InstanceId
+echo ${instance}
+aws ec2 stop-instances ${instance}
+
